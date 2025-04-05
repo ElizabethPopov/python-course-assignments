8bba27e71330c8c2b9bfee05db2f1cf064f8b7cc
Elizabeth Popov <liza.y.popov@gmail.com>
2025-04-05 14:19:38 +0300

    Creating circle.py

---
+++ day02/circle.py
@@ -0,0 +1,8 @@
+from math import pi
+
+radius = float(input("Enter the radius of the circle you want to calculate: "))
+
+area = pi*(radius)**2 
+circumeference = 2*pi*radius
+
+print(f"The area of your circle: {area}\n The circumference of yo circle: {circumeference}")
