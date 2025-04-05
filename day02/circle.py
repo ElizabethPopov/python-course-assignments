c7c8d2fea7bb89ac9c88a648b0cd9715e78ae235
Elizabeth Popov <liza.y.popov@gmail.com>
2025-04-05 14:43:19 +0300

    Update circle.py

--- day02/circle.py
+++ day02/circle.py
@@ -5,4 +5,4 @@ radius = float(input("Enter the radius of the circle you want to calculate: "))
 area = pi*(radius)**2 
 circumeference = 2*pi*radius
 
-print(f"The area of your circle: {round(area,3)}\nThe circumference of yo circle: {round(circumeference,3)}")
+print(f"The area of your circle: {round(area,3)}\nThe circumference of your circle: {round(circumeference,3)}")
