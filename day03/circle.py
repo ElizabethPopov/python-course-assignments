811c194ae96126cfa0fdd5b03a0741e335b17f1b
Elizabeth Popov <liza.y.popov@gmail.com>
2025-04-11 17:25:54 +0300

    updated circle.py

--- day03/circle.py
+++ day03/circle.py
@@ -1,10 +1,15 @@
 from math import pi
 import sys
 
-
 if len(sys.argv) != 2:
     print("You should provide a single numerical value for the radius.")
     sys.exit(1)
+else:
+    try:
+        float(sys.argv[1])
+    except ValueError:
+        print("The radius should be a number.")
+        sys.exit(1)
 
 radius = float(sys.argv[1])
 
