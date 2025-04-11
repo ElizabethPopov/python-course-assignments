8e48581ca8668d1bab4b9bfa0263af64aced53bc
Elizabeth Popov <liza.y.popov@gmail.com>
2025-04-11 17:21:56 +0300

    updated circle.py

--- day03/circle.py
+++ day03/circle.py
@@ -1,11 +1,12 @@
 from math import pi
 import sys
 
-radius = float(sys.argv[1])
 
 if len(sys.argv) != 2:
     print("You should provide a single numerical value for the radius.")
     sys.exit(1)
+
+radius = float(sys.argv[1])
 
 area = pi*(radius)**2 
 circumeference = 2*pi*radius
