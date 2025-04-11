1becca56c3100d84aa9f37789057bf20ae764370
Elizabeth Popov <liza.y.popov@gmail.com>
2025-04-11 17:55:04 +0300

    updated rectangle.py

--- day03/rectangle.py
+++ day03/rectangle.py
@@ -3,13 +3,13 @@ import sys
 if len(sys.argv) != 3:
     print("You should provide two numerical values for the height and width.")
     sys.exit(1)
-else:
-    try:
-        height = float(sys.argv[1])
-        width = float(sys.argv[2])
-    except ValueError:
-        print("The height and width should be numbers.")
-        sys.exit(1)
+
+try:
+    height = float(sys.argv[1])
+    width = float(sys.argv[2])
+except ValueError:
+    print("The height and width should be numbers.")
+    sys.exit(1)
 
 
 area = height * width
