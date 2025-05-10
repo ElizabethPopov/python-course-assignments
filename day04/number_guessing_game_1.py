e21b7c709d4dd5c09a61e2aad75a34402125593b
Elizabeth Popov <liza.y.popov@gmail.com>
2025-05-10 14:12:30 +0300

    create number_guessing_game_1.py

---
+++ day04/number_guessing_game_1.py
@@ -0,0 +1,24 @@
+import random
+import sys
+
+random_num = random.randint(1, 20)
+
+user_input = int(input("Guess a number between 1 and 20: "))
+
+guess = True
+
+if random_num == user_input:
+    print("You guessed the number correctly!")
+
+while not guess:
+
+    if random_num < user_input:
+        print("The number you guessed is bigger than the actual number. Try again!")
+        user_input = int(input("Guess a number between 1 and 20: "))
+
+    else:
+        print("The number you guessed is smaller than the actual number. Try again!")
+        user_input = int(input("Guess a number between 1 and 20: "))
+
+
+    
