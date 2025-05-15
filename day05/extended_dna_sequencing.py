09e377a29d3a3916c804ffd7a5bbb232118d525b
Elizabeth Popov <liza.y.popov@gmail.com>
2025-05-15 21:59:45 +0300

    changed extended_dna_sequencing.py

--- day05/extended_dna_sequencing.py
+++ day05/extended_dna_sequencing.py
@@ -2,6 +2,10 @@ seq_input = input("Enter a DNA sequence: ")
 
 seq_input = seq_input.upper().split('X') 
 
+#seq_input = [seq_input.split(letter) for letter in seq_input if letter not in 'ACGT' else continue]
+
+
+
 seq_input = filter(lambda x: x != '', seq_input)
 
 print(sorted(list(seq_input), key = len, reverse = True))
