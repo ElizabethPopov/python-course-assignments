05d20ba94af459bce6b115fcb2dc26b8f9da6cf8
Elizabeth Popov <liza.y.popov@gmail.com>
2025-05-15 21:45:00 +0300

    changed dna_sequencing.py and created extended_dna_sequencing.py

--- day05/extended_dna_sequencing.py
+++ day05/extended_dna_sequencing.py
@@ -1,10 +1,4 @@
-import sys
-
-if len(sys.argv) != 2:  
-    print("You should provide a single sequence composed of letters.")
-    sys.exit(1)
-
-seq_input = sys.argv[1]
+seq_input = input("Enter a DNA sequence: ")
 
 seq_input = seq_input.upper().split('X') 
 
