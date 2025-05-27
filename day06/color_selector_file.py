2888d80d99a31b2adfebc3d11ed55083e0b7398c
Elizabeth Popov <liza.y.popov@gmail.com>
2025-05-27 22:54:55 +0300

    created color_selector_file.py and colors.txt

---
+++ day06/color_selector_file.py
@@ -0,0 +1,47 @@
+import sys
+
+print("Type 'x' to exit the program.")
+
+if len(sys.argv) != 2:  
+    print("You should provide a txt file.")
+    sys.exit(1)
+
+file_name = sys.argv[1]
+
+with open(file_name, 'r') as file:
+    content = file.read()
+    color_list = content.splitlines()
+
+print("Here are the colors in the file:")
+
+for i, color in enumerate(color_list):
+    print(f"{i+1}. {color_list[i]}")
+
+while True:
+
+    selection = input("What color do you want to select? ")
+
+    if selection.isdigit():
+        try:
+            selection = int(selection)
+            if 1 <= selection <= len(color_list):
+                print(f"You selected: {color_list[selection - 1]}")
+            else:
+                print("Invalid selection. Please select a number from the list.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+    else:
+        selection = selection.lower()
+        if selection in [color.lower() for color in color_list]:
+            print(f"You selected: {selection}")
+        
+        elif selection == 'x':
+            print("Exiting the program.")
+            break
+
+        else:
+            print("Invalid selection. Please select a color from the list.")
+
+
+
