80ad75cbe158e58fddc27233292b8d72ff2d4575
Elizabeth Popov <liza.y.popov@gmail.com>
2025-06-03 22:03:09 +0300

    created extended_dna_sequencing_with_functions.py

---
+++ day07/extended_dna_sequencing_with_functions.py
@@ -0,0 +1,29 @@
+def return_sequence_from_input(seq_input):
+    '''Returns a sequence from the input string, filtering out non-DNA characters.'''
+    seq = ''
+    seq_lst = []
+    for letter in seq_input.upper():
+        if letter in 'ATCG':
+            seq += letter
+        else:
+            if seq:
+                seq_lst.append(seq)
+                seq = ''
+    if seq:
+        seq_lst.append(seq)
+    
+    return seq_lst
+
+def sort_sequences(seq_lst):
+    '''Sorts a list of sequences by length in descending order.'''
+    return sorted(seq_lst, key=len, reverse=True)
+
+def main():
+    '''Main function to execute the sequence processing.'''
+    seq_input = input("Enter a DNA sequence: ")
+    seq_lst = return_sequence_from_input(seq_input)
+    sorted_seq_lst = sort_sequences(seq_lst)
+    print(sorted_seq_lst)
+
+if __name__ == "__main__":
+    main()
